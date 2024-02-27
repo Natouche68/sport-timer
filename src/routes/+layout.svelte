6d64e7f35885177cb3b99930e8bb55240d69b47f
Natouche68 <ngrossheny@gmail.com>
2024-02-27 18:18:15 +0100

    Add a toggle between light and dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import "../app.pcss";
-	import { ModeWatcher } from "mode-watcher";
+	import { ModeWatcher, toggleMode } from "mode-watcher";
 	import { Separator } from "$lib/components/ui/separator";
+	import { Button } from "$lib/components/ui/button";
+	import Sun from "lucide-svelte/icons/sun";
+	import Moon from "lucide-svelte/icons/moon";
 </script>
 
 <svelte:head>
@@ -13,7 +16,18 @@
 <header
 	class="fixed top-0 left-0 right-0 flex flex-col justify-center items-center"
 >
-	<h1 class="my-3 text-2xl font-bold">Sport Timer</h1>
+	<div class="my-3 w-full text-center relative">
+		<h1 class="text-2xl font-bold">Sport Timer</h1>
+		<Button
+			variant="outline"
+			size="icon"
+			class="absolute right-1 -top-1"
+			on:click={toggleMode}
+		>
+			<Sun class="w-4 h-4 block dark:hidden" />
+			<Moon class="w-4 h-4 hidden dark:block" />
+		</Button>
+	</div>
 	<Separator />
 </header>
 
