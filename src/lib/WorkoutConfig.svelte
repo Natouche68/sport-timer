03362f00b0ef32c04b869c7333b495f387e185dd
Natouche68 <ngrossheny@gmail.com>
2024-02-27 17:18:00 +0100

    Show the current exercise

--- src/lib/WorkoutConfig.svelte
+++ src/lib/WorkoutConfig.svelte
@@ -6,6 +6,9 @@
 	import Dumbbell from "lucide-svelte/icons/dumbbell";
 	import Pencil from "lucide-svelte/icons/pencil";
 	import PauseCircle from "lucide-svelte/icons/pause-circle";
+	import Play from "lucide-svelte/icons/play";
+	import Check from "lucide-svelte/icons/check";
+	import Hourglass from "lucide-svelte/icons/hourglass";
 </script>
 
 <Card.Root class="md:w-1/3">
@@ -15,7 +18,7 @@
 	<Card.Content>
 		<Table.Root>
 			<Table.Body>
-				{#each $workoutConfig as exercise}
+				{#each $workoutConfig as exercise, i}
 					<Table.Row>
 						<Table.Cell>
 							{#if exercise.type === "exercise"}
@@ -27,7 +30,17 @@
 						<Table.Cell class="font-medium">{exercise.name}</Table.Cell>
 						<Table.Cell>{exercise.time}s</Table.Cell>
 						<Table.Cell class="text-right">
-							{#if !$currentData.playing}
+							{#if $currentData.playing}
+								<Button disabled variant="ghost" size="icon">
+									{#if $currentData.currentlyDoing == i}
+										<Play />
+									{:else if $currentData.currentlyDoing > i}
+										<Check />
+									{:else if $currentData.currentlyDoing < i}
+										<Hourglass />
+									{/if}
+								</Button>
+							{:else}
 								<Button variant="outline" size="icon">
 									<Pencil class="w-4 h-4" />
 								</Button>
