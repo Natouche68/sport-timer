f605f626cb18da906a9ec6ecda4111e1376c3180
Natouche68 <ngrossheny@gmail.com>
2024-02-29 20:05:01 +0100

    Fix a bug with an empty config on mobile

--- src/lib/WorkoutConfig.svelte
+++ src/lib/WorkoutConfig.svelte
@@ -17,7 +17,7 @@
 
 <Card.Root class="md:w-1/3 mb-8 md:mb-0 md:mt-16">
 	<Card.Header>
-		<Card.Title tag="h2" class="flex justify-between items-center">
+		<Card.Title tag="h2" class="flex justify-between items-center gap-32">
 			<div>My Workout</div>
 			<Drawer.Root>
 				<Drawer.Trigger>
